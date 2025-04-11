13c27e5329d4f885a64c8c9e4ee8913d3edb9b63
aleks <gol8an@yandex.ru>
2025-04-11 17:41:23 +0300

    Разбор первой программы по "взрослому" [#505151]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -4,7 +4,7 @@ public class Multiple {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 9 ; i++) {
+        for (int i = 1; i <= 9; i++) {
             System.out.println(i + " * " + 1 + " = " + 1 * i);
         }
     }
