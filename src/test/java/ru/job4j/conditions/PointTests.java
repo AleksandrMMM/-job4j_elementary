6ac426d682979bcd1a11fcd26d4ce601010339a7
aleks <gol8an@yandex.ru>
2025-05-10 19:04:15 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587]

---
+++ src/test/java/ru/job4j/conditions/PointTests.java
@@ -0,0 +1,19 @@
+package ru.job4j.conditions;
+
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.Test;
+import ru.job4j.condition.Point;
+
+import static org.assertj.core.api.Assertions.withPrecision;
+
+
+public class PointTests {
+
+    @Test
+    public void whenPointsAreSameThenDistanceIsZero() {
+        Point a = new Point(0, 1);
+        Point b = new Point(2, 5);
+        double result = a.distance(b);
+        AssertionsForClassTypes.assertThat(result).isEqualTo(4.472, withPrecision(0.002));
+    }
+}
